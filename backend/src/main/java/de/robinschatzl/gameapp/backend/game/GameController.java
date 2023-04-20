8c96c4280f0b964daca768e0b1d6bca7d1397ce9
Robin Schatzl <tmai02476@gmail.com>
2023-04-20 20:15:27 +0200

    added GameController.java with getAllGames methode

---
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/game/GameController.java
@@ -0,0 +1,21 @@
+package de.robinschatzl.gameapp.backend.game;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/games")
+public class GameController {
+
+    private final GameService gameService
+
+    @GetMapping
+    public List<Game> getAllGames() {
+        return gameService.getAllGames();
+    }
+}
