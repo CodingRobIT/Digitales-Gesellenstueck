9136e5bdafe11835aa5e701dfb030c985592b7ba
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-04-23 08:41:52 +0200

    changed AddGame.tsx now with MUI setup

--- frontend/src/AddGame.tsx
+++ frontend/src/AddGame.tsx
@@ -1,73 +1,71 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import {NewGame} from "./Game";
-import {FormEvent, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { Button, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { NewGame } from './Game';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type AddGameProps = {
-    addGame: (newGame: NewGame) => void
-}
-export default function AddGame(props: AddGameProps) {
+    addGame: (newGame: NewGame) => void;
+};
 
-    const navigate = useNavigate()
-    const [titel , setTitel] = useState<string>('')
-    const [publisher , setPublisher] = useState<string>('')
-    const [genre , setGenre] = useState<string>('')
-    const [note , setNote] = useState<string>('')
+const FormContainer = styled('form')({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1rem',
+    maxWidth: '500px',
+    margin: '0 auto',
+});
+
+export default function AddGame(props: AddGameProps) {
+    const navigate = useNavigate();
+    const [title, setTitle] = useState<string>('');
+    const [publisher, setPublisher] = useState<string>('');
+    const [genre, setGenre] = useState<string>('');
+    const [note, setNote] = useState<string>('');
 
     function onSaveGame(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
+        event.preventDefault();
 
-        const newGame: NewGame = {titel: titel, publisher: publisher, genre: genre, note: note}
+        const newGame: NewGame = {title: title, publisher: publisher, genre: genre, note: note};
 
-        props.addGame(newGame)
+        props.addGame(newGame);
 
-        navigate('/games')
+        navigate('/games');
     }
 
     return (
-        <Form onSubmit={onSaveGame}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Spiel Titel</Form.Label>
-                <Form.Control type="text"
-                              placeholder="Spiel Titel"
-                              value={titel}
-                              onChange={(event) => {
-                                  setTitel(event.target.value)
-                              }}/>
-            </Form.Group>
+        <FormContainer onSubmit={onSaveGame}>
+            <TextField
+                label="Spiel Titel"
+                variant="outlined"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+            />
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Publisher</Form.Label>
-                <Form.Control type="text"
-                              placeholder="Hersteller des Spiels"
-                              value={publisher}
-                              onChange={(event) => {
-                                  setPublisher(event.target.value)
-                              }}/>
-            </Form.Group>
+            <TextField
+                label="Publisher"
+                variant="outlined"
+                value={publisher}
+                onChange={(event) => setPublisher(event.target.value)}
+            />
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Genre</Form.Label>
-                <Form.Control type="text"
-                              placeholder="Zu welchem Genre gehört das Spiel"
-                              value={genre}
-                              onChange={(event) => {
-                                  setGenre(event.target.value)
-                              }}/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Notiz</Form.Label>
-                <Form.Control type="text"
-                              placeholder="Freie Notiz für dich (für welche Platform du das Game besitzt.)"
-                              value={note}
-                              onChange={(event) => {
-                                  setNote(event.target.value)
-                              }}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
+            <TextField
+                label="Genre"
+                variant="outlined"
+                value={genre}
+                onChange={(event) => setGenre(event.target.value)}
+            />
+
+            <TextField
+                label="Notiz"
+                variant="outlined"
+                value={note}
+                onChange={(event) => setNote(event.target.value)}
+            />
+
+            <Button variant="contained" type="submit">
                 Save Game
             </Button>
-        </Form>
+        </FormContainer>
     );
 }
