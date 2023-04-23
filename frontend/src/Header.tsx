8d935b147e78201e7c387f6f820b7b3b3aa0a24a
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-04-23 08:26:04 +0200

    added "Neues game Hinzufügen" in Header.tsx

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,10 +1,12 @@
 import {Link} from "react-router-dom";
+import {NavLink} from "react-bootstrap";
 
 export default function Header() {
     return (
         <div>
             <h1>Hier entsteht meine Video Game Library</h1>
             <Link to="/games">Zu den Games</Link>
+            <Link to="/games/add">Neues Game Hinzufügen</Link>
         </div>
     )
 }
