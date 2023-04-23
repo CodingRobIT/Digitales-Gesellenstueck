fad4117637937839a457f3ea886e103ecc648992
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-04-23 08:53:40 +0200

    changed Header.tsx with MUI

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,12 +1,26 @@
-import {Link} from "react-router-dom";
-import {NavLink} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 export default function Header() {
     return (
-        <div>
-            <h1>Hier entsteht meine Video Game Library</h1>
-            <Link to="/games">Zu den Games</Link>
-            <Link to="/games/add">Neues Game Hinzufügen</Link>
-        </div>
-    )
+        <AppBar position="static" sx={{ bgcolor: "#1E2432" }}>
+            <Toolbar sx={{ justifyContent: "center" }}>
+                <div>
+                    <Typography variant="h6" component="div" align="center">
+                        Hier entsteht meine Video Game Library
+                    </Typography>
+                    <div>
+                        <Typography component="div" align="center">
+                            <Button color="inherit" component={Link} to="/games">
+                                Zu den Games
+                            </Button>
+                            <Button color="inherit" component={Link} to="/games/add">
+                                Neues Game Hinzufügen
+                            </Button>
+                        </Typography>
+                    </div>
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
 }
