637181554e2f7cfee65f8aadddf41238b491169c
Robin Schatzl <tmai02476@gmail.com>
2023-04-24 22:06:59 +0200

    <<TEMP>> update useGameDetail.ts

--- frontend/src/useGameDetail.ts
+++ frontend/src/useGameDetail.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Game} from "./Game";
 import axios from "axios";
 import {toast} from "react-toastify";
@@ -6,25 +6,61 @@ import {useParams} from "react-router-dom";
 
 export default function useGameDetail() {
     const [game, setGame] = useState<Game>();
-    const {id} = useParams<{id: string}>()
+    const {id} = useParams<{ id: string }>();
+    const [editing, setEditing] = useState(false);
+    const [editedGame, setEditedGame] = useState<Game>({
+        id: "",
+        title: "",
+        publisher: "",
+        genre: "",
+        note: "",
+    });
 
     useEffect(() => {
         if (id) {
             loadGameById(id);
         }
         //eslint-disable-next-line
-    },[]);
+    }, []);
 
     function loadGameById(id: string) {
         axios
             .get("/api/games/" + id)
             .then((response) => {
-                setGame(response.data)
+                setGame(response.data);
+                setEditedGame(response.data);
             })
             .catch((error) => {
                 toast.error("Game dose not exist");
             });
     }
 
-    return {game}
+    function editOnClick() {
+        setEditing(true);
+    }
+
+    function gameInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const {name, value} = event.target;
+        setEditedGame((prevGame) => ({
+            ...prevGame,
+            [name]: value,
+        }))
+    }
+
+    function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        axios
+            .put("/api/games/" + id, editedGame)
+            .then((response) => {
+                setGame(response.data);
+                setEditing(false);
+                window.location.reload();
+                toast.success("Game updated successfully");
+            })
+            .catch((error) => {
+                toast.error("Failed to update Game");
+            });
+    }
+
+    return {game, editedGame, editing, handleFormSubmit, editOnClick, gameInputChange}
 }
