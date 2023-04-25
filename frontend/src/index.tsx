481c2c0d243dd0bb4a39834bd47342ce9323637b
Robin Schatzl <tmai02476@gmail.com>
2023-04-25 16:07:38 +0200

    added styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -13,6 +13,16 @@ root.render(
   </React.StrictMode>
 );
 
+const metaThemeColor = document.querySelector("meta[name=theme-color]");
+if (metaThemeColor) {
+    metaThemeColor.setAttribute("content", "#000000");
+}
+
+const metaColorScheme = document.querySelector("meta[name=color-scheme]");
+if (metaColorScheme) {
+    metaColorScheme.setAttribute("content", "dark");
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
