2b178221e6dd71f00c795c0a74beb5720857a714
Robin Schatzl <tmai02476@gmail.com>
2023-05-03 12:15:47 +0200

    added test getMapping im UserController.java

---
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/security/UserController.java
@@ -0,0 +1,15 @@
+package de.robinschatzl.gameapp.backend.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping
+    public String getTest() {
+        return "Test";
+    }
+}
