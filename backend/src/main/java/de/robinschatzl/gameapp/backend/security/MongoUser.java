a6b83b3977828d6f1e93f4a8d66f496d967262d3
Robin Schatzl <tmai02476@gmail.com>
2023-05-03 12:26:56 +0200

    added MongoUser with id, username and paswword also with @Document annotation for DB info how to name the directory.

---
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/security/MongoUser.java
@@ -0,0 +1,13 @@
+package de.robinschatzl.gameapp.backend.security;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("mongoUsers")
+public record MongoUser (
+        @Id
+        String id,
+        String username,
+        String password
+) {
+}
