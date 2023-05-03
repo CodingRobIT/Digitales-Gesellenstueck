cefbce1d52cc04dc02c62f7835ab54af1b86e1dd
Robin Schatzl <tmai02476@gmail.com>
2023-05-03 11:17:26 +0200

    added SecurityFilterChain to SecurityConfig.java

--- backend/src/main/java/de/robinschatzl/gameapp/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package de.robinschatzl.gameapp.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -33,4 +35,17 @@ public class SecurityConfig {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/user").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and().build();
+    }
 }
