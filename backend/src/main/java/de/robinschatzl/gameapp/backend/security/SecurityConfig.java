c53272780bc319cbd92853b8ddbf219f3dbbe369
Robin Schatzl <tmai02476@gmail.com>
2023-05-03 13:47:22 +0200

    cleaning, delete empty unused lines

--- backend/src/main/java/de/robinschatzl/gameapp/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
