05bbc4ef8b06328d2409abfd9ce83a8e7470ba92
Robin Schatzl <tmai02476@gmail.com>
2023-05-04 12:03:03 +0200

    added Login Button to the Header.tsx

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import {Link} from "react-router-dom";
+import {AppBar, Toolbar, Typography, Button} from "@mui/material";
 import './Header.css'
 
 export default function Header() {
     return (
-        <AppBar className="header" position="static" sx={{ bgcolor: "#1E2432" }}>
-            <Toolbar sx={{ justifyContent: "center" }}>
+        <AppBar className="header" position="static" sx={{bgcolor: "#1E2432"}}>
+            <Toolbar sx={{justifyContent: "center"}}>
                 <div>
                     <Typography variant="h6" component="div" align="center">
                         Hier entsteht meine Video Game Library
@@ -18,7 +18,11 @@ export default function Header() {
                             <Button color="inherit" component={Link} to="/games/add">
                                 Neues Game Hinzufügen
                             </Button>
+                            <Button color="inherit" component={Link} to="/login">
+                                Login
+                            </Button>
                         </Typography>
+
                     </div>
                 </div>
             </Toolbar>
