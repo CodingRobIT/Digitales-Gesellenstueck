53173f6dd30bc90d6ab037e529df9b74a23ad02a
Robin Schatzl <tmai02476@gmail.com>
2023-05-12 16:14:20 +0200

    added @PostMapping SignIn function to Controller and Service Class

--- backend/src/main/java/de/robinschatzl/gameapp/backend/security/UserController.java
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/security/UserController.java
@@ -1,18 +1,21 @@
 package de.robinschatzl.gameapp.backend.security;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final MongoUserRepository mongoUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/me")
     public String getMe() {
@@ -39,6 +42,19 @@ public class UserController {
     public void logout(HttpSession httpSession) {
         httpSession.invalidate();
         SecurityContextHolder.clearContext();
+    }
+
+    @PostMapping("/signin")
+    //eslint-disable-next-line
+    public MongoUser signIn(@RequestBody @Valid MongoUser user) {
+        String errorMessage = "Username already exists!";
+
+        if (mongoUserRepository.findMongoUserByUsername(user.username()).isPresent()) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+        String encodedPassword = passwordEncoder.encode(user.password());
+        MongoUser newUser = new MongoUser(null, user.username(), encodedPassword);
+        return mongoUserRepository.save(newUser);
     }
 
 }
--- backend/src/main/java/de/robinschatzl/gameapp/backend/security/UserService.java
+++ backend/src/main/java/de/robinschatzl/gameapp/backend/security/UserService.java
@@ -1,17 +1,33 @@
 package de.robinschatzl.gameapp.backend.security;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
     private final MongoUserRepository mongoUserRepository;
+    private final PasswordEncoder encoder;
+
+    public UserService(MongoUserRepository mongoUserRepository, PasswordEncoder encoder) {
+        this.mongoUserRepository = mongoUserRepository;
+        this.encoder = encoder;
+    }
 
     public MongoUser findUserByUsername(String username) {
         return mongoUserRepository.findMongoUserByUsername(username).orElseThrow(NoSuchElementException::new);
+    }
+
+    public MongoUser createMongoUser(MongoUser mongoUser) {
+
+        if (mongoUserRepository.findMongoUserByUsername(mongoUser.username()).isPresent()) {
+            throw new IllegalArgumentException("The username already exists.");
+        } else {
+            String encodedPassword = encoder.encode(mongoUser.password());
+            MongoUser encodedUser = new MongoUser(mongoUser.id(), mongoUser.username(), encodedPassword);
+            return mongoUserRepository.save(encodedUser);
+        }
     }
 }
