f77abae56aea589e1b88e7d39784a95608fd934c
Robin Schatzl <tmai02476@gmail.com>
2023-05-14 20:42:02 +0200

    bugfix in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,8 +19,7 @@ function App() {
 
     useEffect(() => {
         if (user) {
-            loadAllGames()
-            useGames();
+            loadAllGames();
         }
         //eslint-disable-next-line
     }, []);
