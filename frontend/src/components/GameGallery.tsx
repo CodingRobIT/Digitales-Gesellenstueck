514f97556712f2b537d20a3ccdc303034b124dd7
CodingRobIT <tmai02476@gmail.com>
2026-05-08 08:57:29 +0200

    Update GameGallery.tsx

--- frontend/src/components/GameGallery.tsx
+++ frontend/src/components/GameGallery.tsx
@@ -1,6 +1,5 @@
 import {Game} from "../model/Game";
-import React, {useEffect, useState} from "react";
-import useGames from "../customHooks/useGames";
+import React, {useState} from "react";
 import GameCard from "./GameCard";
 import {TextField} from "@mui/material";
 import './GameGallery.css'
@@ -10,39 +9,32 @@ type GameGalleryProps = {
     games: Game[],
 }
 
-//eslint-disable-next-line
 export default function GameGallery(props: GameGalleryProps) {
-    const {games,loadAllGames} = useGames()
-    const [searchTerm, setSearchTerm] = useState("")
+    const [searchTerm, setSearchTerm] = useState("");
 
-    const filteredGames = games.filter((game) =>
+    const filteredGames = props.games.filter((game) =>
         game.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
-
-    useEffect(() => {
-        loadAllGames().catch(
-            (response) => {
-                console.error(response)
-            }
-        )
-        //eslint-disable-next-line
-    }, []);
 
     return (
         <div className="game-gallery">
             <div className="searchbar">
-                <TextField id="outlined-basic"
-                           label="Games Suchen..."
-                           variant="filled"
-                           value={searchTerm}
-                           InputProps={{sx: {color: "deepskyblue", fontWeight: "bold"}}}
-                           InputLabelProps={{sx: {color: "Snow"}}}
-                           onChange={(e) => setSearchTerm(e.target.value)}/>
-                <div className="game-cards">  {filteredGames.reverse().map((card: Game) => (
-                    <GameCard key={card.id}
-                              game={card}/>
-                ))}
+                <TextField
+                    id="outlined-basic"
+                    label="Games Suchen..."
+                    variant="filled"
+                    value={searchTerm}
+                    InputProps={{sx: {color: "deepskyblue", fontWeight: "bold"}}}
+                    InputLabelProps={{sx: {color: "Snow"}}}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+
+                <div className="game-cards">
+                    {[...filteredGames].reverse().map((card: Game) => (
+                        <GameCard key={card.id} game={card}/>
+                    ))}
                 </div>
+
                 <ToastContainer
                     position="top-right"
                     autoClose={5000}
@@ -57,5 +49,5 @@ export default function GameGallery(props: GameGalleryProps) {
                 />
             </div>
         </div>
-    )
+    );
 }
