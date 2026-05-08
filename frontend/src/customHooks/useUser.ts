d218c0a8ea14312e41145fdf9bd728f849fa0aa6
CodingRobIT <tmai02476@gmail.com>
2026-05-08 09:46:42 +0200

    Update useUser.ts correct catch for error now

--- frontend/src/customHooks/useUser.ts
+++ frontend/src/customHooks/useUser.ts
@@ -36,8 +36,9 @@ export default function useUser() {
                 setUser(response.data);
                 toast.success("Login erfolgreich!");
             })
-            .catch(() => {
+            .catch((error) => {
                 toast.error("Login fehlgeschlagen: Bitte Benutzernamen und/oder Passwort überprüfen.");
+                throw error;
             });
     }
 
